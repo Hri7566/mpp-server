7051cfd9da4855b12f1bbc6eb05b00181d29ef8d
Hri7566 <hri7566@gmail.com>
2021-03-20 19:12:38 +0000

    add dotenv

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = Object.seal({
     port: "8080",
     motd: "You agree to read this message.",
-    _id_PrivateKey: "boppity",
+    _id_PrivateKey: process.env.SALT,
     defaultUsername: "Anonymous",
     defaultRoomColor: "#3b5054",
     defaultLobbyColor: "#19b4b9",
     defaultLobbyColor2: "#801014",
-    adminpass: "adminpass"
+    adminpass: process.env.ADMINPASS
 })
