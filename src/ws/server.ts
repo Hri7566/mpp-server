557df8f972dd8d33265788fce476667af8001072
Hri7566 <hri7566@gmail.com>
2023-09-07 19:14:44 -0400

    Basic message handler

--- src/ws/server.ts
+++ src/ws/server.ts
@@ -1,5 +1,6 @@
 import { App, DEDICATED_COMPRESSOR_8KB } from "uWebSockets.js";
 import { Logger } from "../util/Logger";
+import { createUserID } from "../util/id";
 
 const logger = new Logger("WebSocket Server");
 
@@ -10,6 +11,13 @@ export const app = App()
         maxBackpressure: 1024,
         maxPayloadLength: 8192,
         compression: DEDICATED_COMPRESSOR_8KB,
+
+        open: ws => {
+            const ip = String(ws.getRemoteAddressAsText());
+            const _id = createUserID(ip);
+
+            logger.debug(ip, _id);
+        },
 
         message: (ws, message, isBinary) => {
             const msg = String(message);
