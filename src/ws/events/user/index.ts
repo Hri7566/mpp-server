c06854595875c4016f1ca24755449a4347dcaf32
Hri7566 <hri7566@gmail.com>
2023-09-09 01:41:57 -0400

    Add devices callback

--- src/ws/events/user/index.ts
+++ src/ws/events/user/index.ts
@@ -3,7 +3,9 @@ import { EventGroup, eventGroups } from "../../events";
 export const EVENTGROUP_USER = new EventGroup("user");
 
 import { hi } from "./handlers/hi";
+import { devices } from "./handlers/devices";
 
 EVENTGROUP_USER.add(hi);
+EVENTGROUP_USER.add(devices);
 
 eventGroups.push(EVENTGROUP_USER);
