b938ce9a40d367161d275191bf5c351a788588b1
Hri7566 <hri7566@gmail.com>
2023-09-09 01:38:17 -0400

    Base readline (doesn't run)

--- src/util/readline.ts
+++ src/util/readline.ts
@@ -4,3 +4,16 @@ export const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+rl.setPrompt("mpps> ");
+
+rl.prompt();
+
+rl.on("line", msg => {
+    // TODO readline commands
+    rl.prompt();
+});
+
+rl.on("SIGINT", () => {
+    process.exit();
+});
