943fb1fbccfc6692f892f7d7ca0380c25a0916ce
Hri7566 <hri7566@gmail.com>
2023-10-24 23:59:01 -0400

    Comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+/**
+ * MPP Server 2
+ * for mpp.dev
+ * by Hri7566
+ */
+
+// Preload environment variables
 import env from "./util/env";
+
 // import { app } from "./ws/server";
 import "./ws/server";
 import { Logger } from "./util/Logger";
