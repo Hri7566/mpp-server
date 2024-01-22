db36945ae84fe7217a7399bec3c4443132bfe4c8
Hri7566 <hri7566@gmail.com>
2024-01-22 16:22:36 -0500

    Remove env var call since bun autoloads .env now

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,6 @@
  * by Hri7566
  */
 
-// Preload environment variables
-import env from "./util/env";
-
 // import { app } from "./ws/server";
 import "./ws/server";
 import { Logger } from "./util/Logger";
