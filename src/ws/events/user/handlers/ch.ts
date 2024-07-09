5e9c367ea2b76bb8f928b0167c0902e816d3fe94
Hri7566 <hri7566@gmail.com>
2024-07-09 03:05:35 -0400

    Fix ch message ratelimit and sanitization

--- src/ws/events/user/handlers/ch.ts
+++ src/ws/events/user/handlers/ch.ts
@@ -4,7 +4,8 @@ export const ch: ServerEventListener<"ch"> = {
     id: "ch",
     callback: (msg, socket) => {
         // Switch channel
-        if (!msg._id) return;
+        if (!socket.rateLimits?.normal.ch.attempt()) return;
+        if (typeof msg._id !== "string") return;
 
         // So technical and convoluted...
         socket.setChannel(msg._id, msg.set);
