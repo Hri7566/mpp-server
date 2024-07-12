adf3a2d3fe2268c2dc775ee869eb946bd9406af6
Hri7566 <hri7566@gmail.com>
2024-07-12 17:29:24 -0400

    Add notification config

--- src/ws/events/admin/handlers/notification.ts
+++ src/ws/events/admin/handlers/notification.ts
@@ -1,6 +1,19 @@
 import { ChannelList } from "../../../../channel/ChannelList";
+import { loadConfig } from "../../../../util/config";
 import { ServerEventListener } from "../../../../util/types";
 import { socketsBySocketID } from "../../../Socket";
+
+const config = loadConfig<{
+    allowXSS: boolean;
+    maxDuration: number;
+    defaultDuration: number;
+    allTarget: string;
+}>("config/notifications.yml", {
+    allowXSS: true,
+    maxDuration: 60000,
+    defaultDuration: 7000,
+    allTarget: "all"
+});
 
 export const notification: ServerEventListener<"notification"> = {
     id: "notification",
@@ -8,9 +21,15 @@ export const notification: ServerEventListener<"notification"> = {
         // Send notification to user/channel
         if (typeof msg.targetChannel == "undefined" && typeof msg.targetUser == "undefined") return;
 
+        if (msg.duration) {
+            if (msg.duration > config.maxDuration) msg.duration = config.maxDuration;
+        } else {
+            msg.duration = config.defaultDuration;
+        }
+
         if (typeof msg.targetChannel !== "undefined") {
             for (const ch of ChannelList.getList().values()) {
-                if (ch.getID() == msg.targetChannel) {
+                if (ch.getID() == msg.targetChannel || msg.targetChannel == config.allTarget) {
                     ch.sendNotification(msg);
                 }
             }
